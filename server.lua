42b900004b62d03667d37e518404b7e1898f92a5
JeesusKrisostoomus <42423446+JeesusKrisostoomus@users.noreply.github.com>
2020-10-03 00:18:58 +0300

    Basic code generation
    This is not inserting values to database and this is not tested

--- server.lua
+++ server.lua
@@ -1,3 +1,41 @@
 --[[
-    Server-side File
+    local _source = source
+    local money = 0
+    local bankmoney = 0
+    local identifier = GetPlayerIdentifiers(_source)[1]
+    MySQL.Async.execute("INSERT INTO moneyAccounts (identifier, cash, bank) VALUES (@Identifier,@cash,@bank)", {['@identifier'] = identifier, ['@cash'] = money, ['@bank'] = bankmoney})
 ]]
+local oldPrint = print
+print = function(trash)
+	oldPrint('^2[Redeem Codes] '..trash..'^0')
+end
+
+local uC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+local lC = "abcdefghijklmnopqrstuvwxyz"
+local num = "0123456789"
+local characterSet = uC .. lC .. num
+
+local RandomCode
+
+function RandomCodeGenerator()
+    math.randomseed(os.time())
+    for i = 1, Config.codeLength do
+        local RandomCode = math.random(#characterSet)
+        print("Generated Code: "..RandomCode)
+    end
+end
+
+--[[
+    Register the command to generate the code.
+]]
+RegisterCommand("genCode", function(source, args, rawCommand)
+        if (args[1] == "money") then
+            if (args[2] == "") then
+                print("Invalid Amount")
+            else
+                RandomCodeGenerator()    
+            end
+        else
+            print("Unknown Type. Please make sure u typed everything correctly and that the script is correctly setup.")
+        end
+end, true)
