4998a2c672d35b55b2becd6804be6b375f1962d4
JeesusKrisostoomus <42423446+JeesusKrisostoomus@users.noreply.github.com>
2020-10-02 23:50:09 +0300

    Dependencies, database connection stuff

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,8 +7,15 @@ client_scripts {
     'client.lua'
 }
 server_scripts {
+    '@async/async.lua',
+	'@mysql-async/lib/MySQL.lua',
     'server.lua'
 }
 shared_scripts {
     'config.lua'
+}
+
+dependencies {
+	'async',
+    'mysql-async'
 }
