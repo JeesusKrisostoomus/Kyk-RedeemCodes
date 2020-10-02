67c67f0066e4df2485406c8053d7c219516f3111
JeesusKrisostoomus <42423446+JeesusKrisostoomus@users.noreply.github.com>
2020-10-03 01:16:01 +0300

    Removed useless stuff

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,20 +2,18 @@ fx_version 'cerulean'
 games { 'gta5' }
 
 author 'Jeesus Krisostoomus#7737'
+description 'https://github.com/JeesusKrisostoomus/FiveM-RedeemCodes'
 
-client_scripts {
-    'client.lua'
-}
 server_scripts {
-    '@async/async.lua',
+    	'@async/async.lua',
 	'@mysql-async/lib/MySQL.lua',
-    'server.lua'
+    	'server.lua'
 }
 shared_scripts {
-    'config.lua'
+    	'config.lua'
 }
 
 dependencies {
 	'async',
-    'mysql-async'
+    	'mysql-async'
 }
