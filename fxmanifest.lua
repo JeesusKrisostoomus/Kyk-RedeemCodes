7e21da058459f670c3ef053968a4ed05fb2228c7
JeesusKrisostoomus <42423446+JeesusKrisostoomus@users.noreply.github.com>
2020-10-02 23:46:49 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Jeesus Krisostoomus#7737'
+
+client_scripts {
+    'client.lua'
+}
+server_scripts {
+    'server.lua'
+}
+shared_scripts {
+    'config.lua'
+}
