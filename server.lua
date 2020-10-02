5d45bd321fba147f246b7428ebf48b34f8b2ed88
JeesusKrisostoomus <42423446+JeesusKrisostoomus@users.noreply.github.com>
2020-10-03 00:55:35 +0300

    Added Redeem Command
    NOT TESTED (PROBABLY NOT WORKING)

--- server.lua
+++ server.lua
@@ -3,6 +3,9 @@ print = function(trash)
 	oldPrint('^2[Redeem Codes] '..trash..'^0')
 end
 
+ESX = nil 
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
 local uC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 local lC = "abcdefghijklmnopqrstuvwxyz"
 local num = "0123456789"
@@ -36,3 +39,26 @@ RegisterCommand("genCode", function(source, args, rawCommand)
             print("Unknown Type. Please make sure u typed everything correctly and that the script is correctly setup.")
         end
 end, true)
+
+RegisterCommand("redeem", function(source, args)
+    MySQL.Async.fetchAll('SELECT * FROM `RedeemCodes` WHERE `code` = @code', {
+			['@code'] = args[1]
+	}, function(data)
+            	if (args[1] == data.code) then
+			xPlayer.addAccountMoney('bank', data.amount)
+			TriggerClientEvent('chat:addMessage', {
+  				color = { 0, 255, 0},
+  				multiline = true,
+  				args = {"Success", "Code redeemed successfuly! You just recieved: $"..data.amount}
+			})
+			--DELETE post FROM blog INNER JOIN post WHERE blog.id = post.blog_id;
+			MySQL.Async.execute("DELETE FROM RedeemCodes WHERE code = data.code;")
+		else
+			TriggerClientEvent('chat:addMessage', {
+  				color = { 255, 0, 0},
+  				multiline = true,
+  				args = {"ERROR", "It seems like this code is invalid!"}
+			})
+		end
+    end)    
+end)
