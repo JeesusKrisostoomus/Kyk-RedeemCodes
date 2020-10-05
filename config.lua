4571fdbbe51b508612df59e7ea44b402a97a800c
Hao <xvender7@gmail.com>
2020-10-05 15:23:39 +0300

    First attempt for a alphanumeric generator

--- config.lua
+++ config.lua
@@ -1,8 +1,15 @@
 Config = {}
 
-Config.minNumber = 0000000
-Config.maxNumber = 9999999
-
 --[[
     This generates a number between 0000000 and 9999999
 ]]
+Config.numericGen = false
+Config.minNumber = 0000000
+Config.maxNumber = 9999999
+
+
+--[[
+    This generates an alphanumeric code from letters (A-Z) and numbers (0-9)
+]]
+Config.alphanumericGen = true		-- numericGen must be "false" for this to work
+Config.length = 7					-- length of the string
