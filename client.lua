abbed00b4a5573d114f08276d823b8b8aaaef40c
JeesusKrisostoomus <42423446+JeesusKrisostoomus@users.noreply.github.com>
2020-10-05 03:18:22 +0300

    Some fixes, New command, Suggestions
    - Fixed when redeeming wrong code then everything errors out. (Bad fix but it works)
    - Added multigen command
    - Added chat suggestions for:(multiGen, Redeem, genCode)

---
+++ client.lua
@@ -0,0 +1,14 @@
+AddEventHandler('onResourceStart', function (resourceName)
+    TriggerEvent('chat:addSuggestion', '/multiGen', 'Generate multiple codes at once', {
+        { name="type", help="What code you want to generate. For example: money" },
+        { name="amount", help="How much (in this case money) will the player be rewarded. For example: 200" },
+        { name="count", help="How many keys you want to generate. For Example: 20" }
+    })
+    TriggerEvent('chat:addSuggestion', '/genCode', 'Generate a reward code', {
+        { name="type", help="What code you want to generate. For example: money" },
+        { name="amount", help="How much (in this case money) will the player be rewarded. For example: 200" }
+    })
+    TriggerEvent('chat:addSuggestion', '/redeem', 'Redeem a reward code', {
+        { name="code", help="Enter the code you want to redeem." }
+    })
+end)
--- server.lua
+++ server.lua
@@ -1,7 +1,9 @@
+--[[
 local oldPrint = print
 print = function(trash)
 	oldPrint('^2[Redeem Codes] '..trash..'^0')
 end
+]]
 
 ESX = nil 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
@@ -47,6 +49,48 @@ RegisterCommand("genCode", function(source, args, rawCommand)
     end
 end, true)
 
+RegisterCommand("multiGen", function(source, args, rawCommand)
+	if (args[1] == "money" or args[1] == "Money") then
+        if (args[2] == nil) then
+			if (source == 0) then
+				print("Invalid arguments")
+			else
+				TriggerClientEvent('chat:addMessage', source, { args = { '^7[^1Error^7]^2', "Invalid Arguments." }, color = 255,255,255 })
+			end
+		else
+			if (args[3] == nil) then args[3] = 0 end --[[ I have no clue why 0 == 1, 1 == 2 but hey whatever.]]
+			if (tonumber(args[3]) < 21) then
+				for shit=0, args[3] do
+					MySQL.Async.execute("INSERT INTO RedeemCodes (code, type, amount) VALUES (@code,@type,@amount)", {
+						['@code'] = RandomCodeGenerator(),
+						['@type'] = "money", 
+						['@amount'] = args[2]
+					})
+					print("Code Generated Successfuly! Code: "..RandomCode)
+					RandomCode = ""
+				end
+				if (source ~= 0) then
+					TriggerClientEvent('chat:addMessage', source, { args = { '^7[^2Success^7]^2', "Codes generated successfully! Check the database to see the codes." }, color = 255,255,255 })
+				else
+					print("Codes Generated Successfully!")
+				end
+			else
+				if (source ~= 0) then
+					TriggerClientEvent('chat:addMessage', source, { args = { '^7[^1Error^7]^2', "Chill. Lets not crash the database." }, color = 255,255,255 })
+				else
+					print("Chill. Lets not crash the database.")
+				end
+			end
+        end
+	else
+		if (source ~= 0) then
+			TriggerClientEvent('chat:addMessage', source, { args = { '^7[^1Error^7]^2', "Unknown Type. Please make sure u typed everything correctly and that the script is correctly setup." }, color = 255,255,255 })
+		else
+			print("Unknown Type. Please make sure u typed everything correctly and that the script is correctly setup.")
+		end
+    end
+end, true)
+
 RegisterCommand("redeem", function(source, args, rawCommand)
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
@@ -60,30 +104,65 @@ RegisterCommand("redeem", function(source, args, rawCommand)
     	MySQL.Async.fetchAll('SELECT * FROM `RedeemCodes` WHERE `code` = @code', {
 				['@code'] = args[1]
 		}, function(data)
-			if (args[1] == data[1].code) then
-				if (source ~= 0) then
-					if (data[1].type == "money") then
-						xPlayer.addAccountMoney('bank', tonumber(data[1].amount))
-						TriggerClientEvent('chat:addMessage', source, { args = { '^7[^2Success^7]^2', "Code redeemed successfully! You just recieved: $"..data[1].amount }, color = 255,255,255 })
-						MySQL.Async.execute("DELETE FROM RedeemCodes WHERE code = @code;", {
-							['@code'] = args[1],
-						})
-					else
-						TriggerClientEvent('chat:addMessage', source, { args = { '^7[^2Success^7]^2', "Code redeemed successfully!" }, color = 255,255,255 })
-						MySQL.Async.execute("DELETE FROM RedeemCodes WHERE code = @code;", {
-							['@code'] = args[1],
-						})
-					end
-				else
-					print("Dude you arent supposed to redeem codes as console. Thats against the law.")
-				end
-			else
+			if (json.encode(data) == "[]" or json.encode(data) == "null") then
 				if (source ~= 0) then
 					TriggerClientEvent('chat:addMessage', source, { args = { '^7[^1Error^7]^2', "Invalid Code!" }, color = 255,255,255 })
 				else
-					print("Invalid Code")
+					print("You cant redeem codes as console!")
+				end
+			else
+				if (args[1] == data[1].code) then
+					if (source ~= 0) then
+						if (data[1].type == "money") then
+							xPlayer.addAccountMoney('bank', tonumber(data[1].amount))
+							TriggerClientEvent('chat:addMessage', source, { args = { '^7[^2Success^7]^2', "Code redeemed successfully! You just recieved: $"..data[1].amount }, color = 255,255,255 })
+							MySQL.Async.execute("DELETE FROM RedeemCodes WHERE code = @code;", {
+								['@code'] = args[1],
+							})
+						else
+							TriggerClientEvent('chat:addMessage', source, { args = { '^7[^2Success^7]^2', "Code redeemed successfully!" }, color = 255,255,255 })
+							MySQL.Async.execute("DELETE FROM RedeemCodes WHERE code = @code;", {
+								['@code'] = args[1],
+							})
+						end
+					else
+						print("Dude you arent supposed to redeem codes as console. Thats against the law.")
+					end
+				else
+					if (source ~= 0) then
+						TriggerClientEvent('chat:addMessage', source, { args = { '^7[^1Error^7]^2', "Invalid Code!" }, color = 255,255,255 })
+					else
+						print("Invalid Code")
+					end
 				end
 			end
 		end)
 	end
 end, false)
+
+--[[
+if (args[1] == data[1].code) then
+	if (source ~= 0) then
+		if (data[1].type == "money") then
+			xPlayer.addAccountMoney('bank', tonumber(data[1].amount))
+			TriggerClientEvent('chat:addMessage', source, { args = { '^7[^2Success^7]^2', "Code redeemed successfully! You just recieved: $"..data[1].amount }, color = 255,255,255 })
+			MySQL.Async.execute("DELETE FROM RedeemCodes WHERE code = @code;", {
+				['@code'] = args[1],
+			})
+		else
+			TriggerClientEvent('chat:addMessage', source, { args = { '^7[^2Success^7]^2', "Code redeemed successfully!" }, color = 255,255,255 })
+			MySQL.Async.execute("DELETE FROM RedeemCodes WHERE code = @code;", {
+				['@code'] = args[1],
+			})
+		end
+	else
+		print("Dude you arent supposed to redeem codes as console. Thats against the law.")
+	end
+else
+	if (source ~= 0) then
+		TriggerClientEvent('chat:addMessage', source, { args = { '^7[^1Error^7]^2', "Invalid Code!" }, color = 255,255,255 })
+	else
+		print("Invalid Code")
+	end
+end
+]]
